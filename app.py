d1c4a0beed1f4447b62596085229d80814a31293
Satyajeet Das <2028162@kiit.ac.in>
2023-08-29 02:29:45 +0530

    update port

--- app.py
+++ app.py
@@ -127,5 +127,8 @@ def upload_image():
     # return jsonify({"message": analysis_result}), 200
 
 
+# if __name__ == '__main__':p
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
